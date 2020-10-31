869f2ad31aabcae8cedfa474699ddc7a860287a2
mac <development@baianat.com>
2020-10-31 22:45:09 +0200

    Add pods to .gitignore

--- Example/Pods/Target Support Files/BT-OtpView/BT-OtpView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BT_OtpView : NSObject
-@end
-@implementation PodsDummy_BT_OtpView
-@end
--- Example/Pods/Target Support Files/Pods-BT-OtpView_Example/Pods-BT-OtpView_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BT_OtpView_Example : NSObject
-@end
-@implementation PodsDummy_Pods_BT_OtpView_Example
-@end
