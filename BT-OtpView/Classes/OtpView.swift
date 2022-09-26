e2f2d19815e5096b196b836b8af9902b60406e9f
Abdelrhman Elbosaty <adbelrahman@baianat.com>
2022-09-26 18:23:14 +0200

    Fix: pasting sms code at first digit

--- BT-OtpView/Classes/OtpView.swift
+++ BT-OtpView/Classes/OtpView.swift
@@ -125,76 +125,96 @@ extension OtpView {
             $0?.delegate = self
             $0?.backActionDelegate = self
         })
+//        firstField.delegate = self
+        
+        if #available(iOS 12.0, *) {
+            firstField.textContentType = .oneTimeCode
+        }
+        self.firstField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        self.firstField.becomeFirstResponder()
     }
 }
 
 // MARK: TextField Delegate
 extension OtpView: UITextFieldDelegate, OtpTextFieldDelegate {
 
-    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        textField.text = ""
-        if string.count <= 1 {
-            textField.text = string
-        } else if string.count >= 4 {
-            let firstCharIdx = string.startIndex
-            let secondCharIdx = string.index(after: firstCharIdx)
-            let thirdCharIdx = string.index(after: secondCharIdx)
-            let fourthCharIdx = string.index(after: thirdCharIdx)
-            firstField.text = String(string[firstCharIdx])
-            secondField.text = String(string[secondCharIdx])
-            thirdField.text = String(string[thirdCharIdx])
-            fourthField.text = String(string[fourthCharIdx])
-
-            [firstField, secondField, thirdField, fourthField].forEach({
-                $0?.resignFirstResponder()
-            })
-            if delegate?.shouldReceive(otpCode: verificationCode, at: self) == true {
-                fourthField.endEditing(true)
-                fourthField.resignFirstResponder()
-            } else {
-                fourthField.becomeFirstResponder()
-            }
-            return false
-        } else {
-            return false
-        }
-
-        let  char = string.cString(using: String.Encoding.utf8)!
-        let isBackSpace = strcmp(char, "\\b")
-
-        if isBackSpace == -92 {
-            highlightPreviousFiled(currentTextField: textField as! OtpTextField)
-        } else {
-
-            func shouldFireDelegate() -> Bool {
-                return verificationCode.count == 4
-            }
-
-            switch textField {
-            case self.firstField:
-                self.secondField.becomeFirstResponder()
-            case self.secondField:
-                self.thirdField.becomeFirstResponder()
-            case self.thirdField:
-                self.fourthField.becomeFirstResponder()
-            case self.fourthField :
-                if shouldFireDelegate() {
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if #available(iOS 12.0, *) {
+            
+            if textField.textContentType == UITextContentType.oneTimeCode{
+                //here split the text to your four text fields
+                if let otpCode = textField.text, otpCode.count > 3 {
+                    firstField.text = String(otpCode[otpCode.index(otpCode.startIndex, offsetBy: 0)])
+                    secondField.text = String(otpCode[otpCode.index(otpCode.startIndex, offsetBy: 1)])
+                    thirdField.text = String(otpCode[otpCode.index(otpCode.startIndex, offsetBy: 2)])
+                    fourthField.text = String(otpCode[otpCode.index(otpCode.startIndex, offsetBy: 3)])
+                                        
                     if delegate?.shouldReceive(otpCode: verificationCode, at: self) == true || textField.isSecureTextEntry {
                         fourthField.endEditing(true)
                         self.fourthField.resignFirstResponder()
                     }
                 }
-            default:
-                break
             }
-        }
-
-        guard let textFieldText = textField.text, let rangeOfTextToReplace = Range(range, in: textFieldText) else {
+         }
+      }
+    
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if (string.count > 3) {
+            firstField.text = String(string[string.index(string.startIndex, offsetBy: 0)])
+            secondField.text = String(string[string.index(string.startIndex, offsetBy: 1)])
+            thirdField.text = String(string[string.index(string.startIndex, offsetBy: 2)])
+            fourthField.text = String(string[string.index(string.startIndex, offsetBy: 3)])
+            
+            fourthField.becomeFirstResponder()
+            
+            if delegate?.shouldReceive(otpCode: verificationCode, at: self) == true || textField.isSecureTextEntry {
+                fourthField.endEditing(true)
+            }
+            
             return false
         }
-        let substringToReplace = textFieldText[rangeOfTextToReplace]
-        let count = textFieldText.count - substringToReplace.count + string.count
-        return count <= 1 && string.count <= 1 || textField.isSecureTextEntry
+        else if (string.count == 1){
+            if textField == firstField {
+                secondField?.becomeFirstResponder()
+            }
+            if textField == secondField {
+                thirdField?.becomeFirstResponder()
+            }
+            if textField == thirdField {
+                fourthField?.becomeFirstResponder()
+            }
+            if textField == fourthField {
+                fourthField?.resignFirstResponder()
+                textField.text? = string
+                
+                if delegate?.shouldReceive(otpCode: verificationCode, at: self) == true || textField.isSecureTextEntry {
+                    fourthField.endEditing(true)
+                    self.fourthField.resignFirstResponder()
+                }
+                
+            }
+            textField.text? = string
+            return false
+        }
+        else{
+            if textField == firstField {
+                firstField?.becomeFirstResponder()
+            }
+            if textField == secondField {
+                firstField?.becomeFirstResponder()
+            }
+            if textField == thirdField {
+                secondField?.becomeFirstResponder()
+            }
+            if textField == fourthField {
+                thirdField?.becomeFirstResponder()
+            }
+            
+            textField.text? = string
+            return false
+        }
+        
     }
 
     public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
